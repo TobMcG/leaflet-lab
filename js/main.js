0137337072a98fb5847248f75e02d44173586154
Tobin McGilligan <mcgilligan2@wisc.edu>
2016-03-02 08:23:19 -0600

    activity 5 part 2 commit
    completed

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 function createMap(){
 
   var map = L.map('map', {
-      center: [43.075865, -89.400984],
+      center: [37.729986, -96.426741],
       zoom: 4,
   });
 
@@ -28,6 +28,7 @@ function getData(map){
             var priceToRentRatiosClusterGroup = L.markerClusterGroup({
               showCoverageOnHover: false,
               maxClusterRadius: 20,
+              removeOutsideVisibleBounds: false,
             });
             var priceToRentRatiosGeoJSON = L.geoJson(response, geoJsonFunctionOptions);
             //add the JSON to the Cluster group, and the cluster to the map
@@ -49,18 +50,36 @@ var geoJsonFunctionOptions = {
       return L.circleMarker(latlng, {
         //circleMarkerOptions
         radius: calcPropRadius(feature.properties['2015-12']),
-        fillColor: "#ff7800",
+        fillColor: '#ff7800',
         color: "#000",
         weight: 1,
         opacity: 1,
-        fillOpacity: 0.8
+        fillOpacity: 1,
       });
     },
     onEachFeature: onEachFeature,
-    /*filter: function(feature, layer) {
-        return feature.properties.Pop_2015 > 20;
-    },*/
 };
+
+function calcSymbolColor(attValue) {
+
+  var color;
+
+  if (attValue <= 10) {
+      color = '#feedde';
+    } else if (attValue > 10 && attValue <= 12.5) {
+      color = '#fdbe85';
+    } else if (attValue > 12.5 && attValue <= 15) {
+      color = '#fd8d3c';
+    } else if (attValue > 15 && attValue <= 17.5) {
+      color = '#e6550d';
+    } else if (attValue > 17.5) {
+      color = '#a63603';
+    } else {
+      console.log('attribute value not accounted for!');
+    }
+
+    return color;
+}
 
 function calcPropRadius(attValue) {
 
@@ -87,6 +106,8 @@ function calcPropRadius(attValue) {
 
     return radius;
 };
+
+
 
 function generatePopup(feature, attribute) {
   return '<strong>' + feature.properties['toGeocode'] + '</strong>' + '</br>'
@@ -118,9 +139,6 @@ function onEachFeaturePopupSetup(layer, popupContent) {
       //do this, despite the fact that the popup is no longer bound
       //to the marker, tehehehe ;)
     },
-    click: function() {
-      $('#dialog').html(popupContent);
-    },
   });
 }
 
@@ -147,62 +165,125 @@ function setupElements(map, attributes) {
   // DIALOG //
   $('#dialog').dialog({
     width: 300,
-    dialogClass: 'moreInfo',
+    dialogClass: 'legend',
     resizable: false,
-    closeText: 'Dock',
+    closeText: 'Expand',
     beforeClose: function () {
-        $(this).dialog('option', 'position',
-          { my: "right bottom", at: "right-10 bottom-27.5", of: window});
-        return false;
+      window.alert('Sorry, this functionality has not been implemented yet!');
+      return false;
     },
-    position: { my: "right bottom", at: "right-10 bottom-27.5", of: window},
   });
-  $('#dialog').dialog('open');
+  $('#dialog').html(
+    '<div id="legendFlexContainer">'
+      +'<div id="attributeContainer">'
+        +'<div>Price-to-Rent</br>Ratio <span id="attrField">2015-12</span></div>'
+      +'</div>'
+      +'<div id="legendSymbols">'
+        +'<div class="s3 circle legendRow"></div>'
+        +'<div class="s2 circle legendRow"></div>'
+        +'<div class="s1 circle legendRow"></div>'
+      +'</div>'
+      +'<div id="legendLabels">'
+        +'<div class="s3 label legendRow"><div>19</div></div>'
+        +'<div class="s2 label legendRow"><div>14</div></div>'
+        +'<div class="s1 label legendRow"><div>9</div></div>'
+      +'</div>'
+    +'</div>'
+  );
+  $('#dialog').dialog('open').dialog('option', 'position',
+    { my: "left bottom", at: "left+10 bottom-10", of: window}
+  );
 
   // Modifying the close buttons behavior and adding another one
   //this first line selects the existing close button
-  $('.moreInfo .ui-dialog-titlebar .ui-dialog-titlebar-close[title="Dock"] .ui-icon')
+  $('.legend .ui-dialog-titlebar .ui-dialog-titlebar-close[title="Expand"] .ui-icon')
     //remove extraneous classes
     .removeClass('ui-icon-closethick').removeClass('ui-button-icon-primary')
-    .addClass('ui-icon-carat-1-se'); //add this class, which has a different symbol
+    .addClass('ui-icon-plus'); //add this class, which has a different symbol
   // Here, we clone the existing close button, and append the copy
-  $('.moreInfo .ui-dialog-titlebar').append(
-    $('.moreInfo .ui-dialog-titlebar-close').clone()
+  $('.legend .ui-dialog-titlebar').append(
+    $('.legend .ui-dialog-titlebar-close').clone()
     //then we move it over a bit and change its title attribute so we can identify it
-    .css('right', '28px').attr('title', 'Expand')
+    .css('right', '28px').attr('title', 'Dock')
   );
   // Now, we select the icon within the cloned copy
-  $('.moreInfo .ui-dialog-titlebar .ui-dialog-titlebar-close[title="Expand"] .ui-icon')
-    .removeClass('ui-icon-carat-1-sw').addClass('ui-icon-plus') //and change its classes
+  $('.legend .ui-dialog-titlebar .ui-dialog-titlebar-close[title="Dock"] .ui-icon')
+    .removeClass('ui-icon-plus').addClass('ui-icon-carat-1-sw') //and change its classes
     //add an event listener & function
     .on('click', function() {
-      window.alert('Sorry, this functionality has not been implemented yet!');
-    });
+      $('#dialog').dialog('option', 'position',
+        { my: "left bottom", at: "left+10 bottom-10", of: window});
+  });
+
+  //hide this wierd empty text that increases the clickable area of the buttons
+  $('.ui-dialog-titlebar-close[title] .ui-button-text').hide();
+
+  var symbolization = 'size';
+  $('.legend .ui-dialog-titlebar .ui-dialog-title').append(
+    '<div id="symbolizationPicker" style="display: inline;">'
+      +'<input type="radio" id="size" name="symbolization" checked>'
+      +'<label for="size">Size</label>'
+      +'<input type="radio" id="color" name="symbolization">'
+      +'<label for="color">Color</label>'
+    +'</div>'
+  );
+  $('#symbolizationPicker').buttonset();
 
   // SLIDER //
+  function updateProportionalSymbols(ui) {
+    var attribute = ui.value-1;
+    // Update Proportional Symbols
+    map.eachLayer(function(layer) {
+      $('#attrField').html(attributes[attribute]);
+      //if the layer is a marker cluster group
+      if (layer.getChildCount) {
+        //do this for all markers in the cluster
+        var markers = layer.getAllChildMarkers();
+        for (i = 0; i < markers.length; i++) {
+          updateProportionalSymbolsOneLayer(markers[i], attribute);
+        }
+      }
+      //if the layer is a feature (not in a cluster) AND has the given property
+      else if (layer.feature && layer.feature.properties[attributes[attribute]]) {
+        updateProportionalSymbolsOneLayer(layer, attribute);
+      } else {
+        //console.log('layer was neither a cluster nor a marker');
+      }
+    });
+  }
+
+  function updateProportionalSymbolsOneLayer(layer, attribute) {
+    //access feature properties
+    var props = layer.feature.properties;
+
+    if (symbolization === 'size') {
+      layer.setStyle({fillColor: '#ff7800'});
+      //update each feature's radius based on new attribute values
+      var radius = calcPropRadius(props[attributes[attribute]]);
+      layer.setRadius(radius);
+    } else if (symbolization === 'color') {
+      layer.setRadius(7);
+      //update each feature's color based on new attribute values
+      var color = calcSymbolColor(props[attributes[attribute]]);
+      layer.setStyle({fillColor: color});
+    } else {
+      console.log('erroneous symbolization!');
+    }
+
+    var popupContent = generatePopup(layer.feature, attributes[attribute]);
+    onEachFeaturePopupSetup(layer, popupContent);
+  }
+
   $( "#slider" ).slider({
     min: 1,
     max: 63,
     value: 63,
     step: 1,
+    slide: function(event, ui) {
+      updateProportionalSymbols(ui);
+    },
     change: function(event, ui) {
-      //console.log('value changed to ' + ui.value);
-      var attribute = ui.value-1;
-      // Update Proportional Symbols
-      map.eachLayer(function(layer) {
-        //if the layer is a feature AND has the given property
-        if (layer.feature && layer.feature.properties[attributes[attribute]]) {
-          //access feature properties
-          var props = layer.feature.properties;
-
-          //update each feature's radius based on new attribute values
-          var radius = calcPropRadius(props[attributes[attribute]]);
-          layer.setRadius(radius);
-
-          var popupContent = generatePopup(layer.feature, attributes[attribute]);
-          onEachFeaturePopupSetup(layer, popupContent);
-        }
-      });
+      updateProportionalSymbols(ui);
     },
   });
   $( "#slider, #slider span" ).on('mouseover', function() {
@@ -233,6 +314,53 @@ function setupElements(map, attributes) {
   //fast forward
   $('#slider-buttons .fa-fast-forward').on('click', function() {
     $('#slider').slider('value', $('#slider').slider('option', 'max'));
+  });
+
+  $('#symbolizationPicker label[for="size"]').on('click', function() {
+    symbolization = 'size';
+    $('#dialog').html(
+      '<div id="legendFlexContainer">'
+        +'<div id="attributeContainer">'
+          +'<div>Price-to-Rent</br>Ratio <span id="attrField">2015-12</span></div>'
+        +'</div>'
+        +'<div id="legendSymbols">'
+          +'<div class="s3 circle legendRow"></div>'
+          +'<div class="s2 circle legendRow"></div>'
+          +'<div class="s1 circle legendRow"></div>'
+        +'</div>'
+        +'<div id="legendLabels">'
+          +'<div class="s3 label legendRow"><div>19</div></div>'
+          +'<div class="s2 label legendRow"><div>14</div></div>'
+          +'<div class="s1 label legendRow"><div>9</div></div>'
+        +'</div>'
+      +'</div>'
+    );
+    updateProportionalSymbols( {value: $('#slider').slider('value')} );
+  });
+  $('#symbolizationPicker label[for="color"]').on('click', function() {
+    symbolization = 'color';
+    $('#dialog').html(
+      '<div id="legendFlexContainer">'
+        +'<div id="attributeContainer">'
+          +'<div>Price-to-Rent</br>Ratio <span id="attrField">2015-12</span></div>'
+        +'</div>'
+        +'<div id="legendSymbols">'
+          +'<div class="c5 circle legendRow"></div>'
+          +'<div class="c4 circle legendRow"></div>'
+          +'<div class="c3 circle legendRow"></div>'
+          +'<div class="c2 circle legendRow"></div>'
+          +'<div class="c1 circle legendRow"></div>'
+        +'</div>'
+        +'<div id="legendLabels">'
+          +'<div class="c5 label legendRow"><div>< 10</div></div>'
+          +'<div class="c4 label legendRow"><div>10 - 12.5</div></div>'
+          +'<div class="c3 label legendRow"><div>12.5 - 15</div></div>'
+          +'<div class="c2 label legendRow"><div>15 - 17.5</div></div>'
+          +'<div class="c1 label legendRow"><div>> 17.5</div></div>'
+        +'</div>'
+      +'</div>'
+    );
+    updateProportionalSymbols( {value: $('#slider').slider('value')} );
   });
 }
 
