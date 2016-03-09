6fcc3bd13b34534a60f00f79d1184e381150827e
Tobin McGilligan <mcgilligan2@wisc.edu>
2016-03-08 23:12:47 -0600

    leaflet lab assignment completed
    leaflet lab assignment completed

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function createMap(){
   });
 
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery   <a href="http://mapbox.com">Mapbox</a>',
+      attribution: 'Housing Data from <a href="http://www.zillow.com/research/data/">Zillow</a>, Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery   <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 10,
       id: 'tobinmcg.p4biih0o',
       accessToken: 'pk.eyJ1IjoidG9iaW5tY2ciLCJhIjoiZWViZjgzODg4NGJiODc3YTQ4YjVhNmQ1NTQ2OTE3ODYifQ.IQc9cPbv4yG-kTMVmkiU7g'
